a1a3f1aaa4e29bd68e763da262b2733cf1dbb8a7
Dave Paroulek <upgradingdave@gmail.com>
2024-07-25 09:00:00 -0400

    Add deploy script

---
+++ deploy2.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+TEMP_SITE="../temp-site"
+
+# (cd ./site; yarn; yarn build)
+
+#mkdir -p "$TEMP_SITE"
+#rsync -avu --delete "./site/build/" "$TEMP_SITE/site"
+#rsync -avu --delete "./img/" "$TEMP_SITE/img"
+#cp ./index.html "$TEMP_SITE/index.html"
+#cp ./app.css "$TEMP_SITE/app.css"
+
+# git checkout main
+
+rsync -avu --delete "$TEMP_SITE/site/" "./site"
+rsync -avu --delete "$TEMP_SITE/img/" "./img"
+cp "$TEMP_SITE/index.html" "./index.html"
+cp "$TEMP_SITE/app.css" "./app.css"
+
+    
