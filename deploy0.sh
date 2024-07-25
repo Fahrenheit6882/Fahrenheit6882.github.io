985ba64bf33bf46914d271ed4846a9f6145ffc13
Dave Paroulek <upgradingdave@gmail.com>
2024-07-25 09:25:24 -0400

    Update deploy scripts

--- deploy0.sh
+++ deploy0.sh
@@ -5,15 +5,15 @@ TEMP_SITE="../temp-site"
 (cd ./site; yarn; yarn build)
 
 #mkdir -p "$TEMP_SITE"
-#rsync -avu --delete "./site/build/" "$TEMP_SITE/site"
-#rsync -avu --delete "./img/" "$TEMP_SITE/img"
+#rsync -av --delete "./site/build/" "$TEMP_SITE/site"
+#rsync -av --delete "./img/" "$TEMP_SITE/img"
 #cp ./index.html "$TEMP_SITE/index.html"
 #cp ./app.css "$TEMP_SITE/app.css"
 
 # git checkout main
 
-#rsync -avu --delete "$TEMP_SITE/site/" "./site"
-#rsync -avu --delete "$TEMP_SITE/img/" "./img"
+#rsync -av --delete "$TEMP_SITE/site/" "./site"
+#rsync -av --delete "$TEMP_SITE/img/" "./img"
 #cp "$TEMP_SITE/index.html" "./index.html"
 #cp "$TEMP_SITE/app.css" "./app.css"
 
